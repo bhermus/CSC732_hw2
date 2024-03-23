7c8c3c678f0384fc2166f51382e7fe283b259017
Ben Hermus <benjamin.hermus@gmail.com>
2024-03-23 14:03:52 -0400

    Load from files instead of library

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from ucimlrepo import fetch_ucirepo
+import matplotlib.pyplot as plt
+import pandas as pd
 
-# fetch dataset
-energy_efficiency = fetch_ucirepo(id=242)
+# Adjust pandas settings
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 
-# data (as pandas dataframes)
-X = energy_efficiency.data.features
-y = energy_efficiency.data.targets
+# Load dataset
+X = pd.read_csv('energy_efficiency_features.csv')
+y = pd.read_csv('energy_efficiency_targets.csv')
 
-# metadata
-print(energy_efficiency.metadata)
+# Set variable information
+feature_descriptions = {
+    'X1': 'Relative Compactness',
+    'X2': 'Surface Area',
+    'X3': 'Wall Area',
+    'X4': 'Roof Area',
+    'X5': 'Overall Height',
+    'X6': 'Orientation',
+    'X7': 'Glazing Area',
+    'X8': 'Glazing Area Distribution'
+}
 
 # variable information
 print(energy_efficiency.variables)
